cd051ba7d7dbbefda975c54c2660101a051b2e75
dreamflyfengzi <fengzi.dreamlfy@gmail.com>
2020-10-17 19:01:37 +0800

    oauth mine box

--- onesgamemine/onesgamemine.cpp
+++ onesgamemine/onesgamemine.cpp
@@ -1,4 +1,5 @@
 #include "onesgamemine.hpp"
+
 #include <eosiolib/transaction.hpp>
 
 #define EOS_TOKEN_SYMBOL symbol("EOS", 4)
@@ -7,12 +8,24 @@
 #define ONES_DEFI_ACCOUNT "onesgamedefi"
 #define ONES_PLAY_ACCOUNT "onesgameplay"
 
+#define BOX_TOKEN_ACCOUNT "token.defi"
+#define BOX_TOKEN_SYMBOL symbol("BOX", 6)
+
+#define DFS_TOKEN_ACCOUNT "minedfstoken"
+#define DFS_TOKEN_SYMBOL symbol("DFS", 4)
+
+const symbol token_symbols[] = {ONES_TOKEN_SYMBOL, BOX_TOKEN_SYMBOL,
+                                DFS_TOKEN_SYMBOL};
+
 uint64_t default_swap_time = 1598961600;
+
+const uint64_t TOKENNUM = 3;
 
 #define ACCOUNT_CHECK(account) \
     eosio_assert(is_account(account), "invalid account " #account);
 
-void split(const std::string &str, char delimiter, std::vector<std::string> &params)
+void split(const std::string &str, char delimiter,
+           std::vector<std::string> &params)
 {
     std::size_t cur, prev = 0;
     cur = str.find(delimiter);
@@ -25,9 +38,131 @@ void split(const std::string &str, char delimiter, std::vector<std::string> &par
     params.push_back(str.substr(prev));
 }
 
+uint64_t get_token_offset(symbol type)
+{
+    for (int i = 0; i < TOKENNUM; i++)
+    {
+        if (token_symbols[i] == type)
+        {
+            return i;
+        }
+    }
+    eosio_assert(false, "invalid token type");
+    return 99999999999999;
+}
+
+void init_amounts(vector<uint64_t> &array)
+{
+    for (int i = array.size(); i < TOKENNUM; i++)
+    {
+        array.push_back(0);
+    }
+}
+
+void add_amounts(vector<uint64_t> &array1, vector<uint64_t> &array2)
+{
+    init_amounts(array1);
+    for (int i = 0; i < array2.size(); i++)
+    {
+        array1[i] += array2[i];
+    }
+}
+
+void reset_amounts(vector<uint64_t> &array)
+{
+    for (int i = 0; i < array.size(); i++)
+    {
+        array[i] = 0;
+    }
+    init_amounts(array);
+}
+void onesgame::transfer(name from, name to, asset quantity, string memo)
+{
+    ACCOUNT_CHECK(from);
+    ACCOUNT_CHECK(to);
+
+    require_auth(from);
+
+    if (from == get_self() || to != get_self())
+    {
+        return;
+    }
+
+    if (quantity.symbol == BOX_TOKEN_SYMBOL)
+    {
+        st_defi_config defi_config = _defi_config.get();
+        defi_config.market_quantity[1] += quantity.amount;
+        _defi_config.set(defi_config, _self);
+    }
+}
+
+void onesgame::init()
+{
+    require_auth(eosio::name(ONES_PLAY_ACCOUNT));
+
+    return;
+}
+
+void onesgame::upgrade()
+{
+    require_auth(eosio::name(ONES_PLAY_ACCOUNT));
+
+    return;
+}
+
+void onesgame::oauth(name account, uint8_t status)
+{
+    require_auth(account);
+
+    auto it = _defi_account.find(account.value);
+    if (it != _defi_account.end())
+    {
+        _defi_account.modify(it, _self, [&](auto &t) {
+            t.oauth = status > 0 ? 1 : 0;
+        });
+    }
+
+    return;
+}
+
+void onesgame::_transfer_to(name to, uint64_t amount, symbol coin_code,
+                            string memo)
+{
+    if (amount == 0)
+    {
+        return;
+    }
+
+    if (coin_code == ONES_TOKEN_SYMBOL)
+    {
+        eosio::action(
+            permission_level{get_self(), "active"_n},
+            eosio::name(ONES_TOKEN_ACCOUNT), "transfer"_n,
+            make_tuple(get_self(), to, asset(amount, ONES_TOKEN_SYMBOL), memo))
+            .send();
+    }
+    else if (coin_code == BOX_TOKEN_SYMBOL)
+    {
+        eosio::action(
+            permission_level{get_self(), "active"_n},
+            eosio::name(BOX_TOKEN_ACCOUNT), "transfer"_n,
+            make_tuple(get_self(), to, asset(amount, BOX_TOKEN_SYMBOL), memo))
+            .send();
+    }
+    else if (coin_code == DFS_TOKEN_SYMBOL)
+    {
+        eosio::action(
+            permission_level{get_self(), "active"_n},
+            eosio::name(DFS_TOKEN_ACCOUNT), "transfer"_n,
+            make_tuple(get_self(), to, asset(amount, DFS_TOKEN_SYMBOL), memo))
+            .send();
+    }
+}
+
 void onesgame::issue(name to, asset quantity, string memo)
 {
     require_auth(eosio::name(ONES_PLAY_ACCOUNT));
+    // eosio_assert(false, "onesgamemine contact is upgrading");
 
     std::vector<std::string> params;
     split(memo, ',', params);
@@ -42,41 +177,13 @@ void onesgame::issue(name to, asset quantity, string memo)
         uint64_t round = atoll(params.at(1).c_str());
         uint64_t total = atoll(params.at(2).c_str());
 
-        auto it = _defi_market.find(round);
-        eosio_assert(it == _defi_market.end(), "invalid round");
-
         uint64_t cur_time = now();
         st_defi_config defi_config = _defi_config.get();
 
-        uint64_t amount = defi_config.market_quantity; // (defi_config.swap_suply - defi_config.last_swap_suply) / 2;
-
-        // uint64_t amount = 100 * 10000;
-        _defi_market.emplace(get_self(), [&](auto &t) {
-            t.round = round;
-            t.quantity = asset(amount, quantity.symbol);
-            t.total = total;
-            t.executed = 0;
-            t.timestamp = cur_time;
-        });
-
         defi_config.market_time = (cur_time / 3600) * 3600;
         defi_config.market_issue += quantity.amount;
-        defi_config.market_suply += amount;
+
         defi_config.last_swap_suply = defi_config.swap_suply;
-        defi_config.market_quantity = 0;
-
-        auto maxit = _defi_market.rbegin();
-        if (maxit != _defi_market.rend())
-        {
-            uint64_t max_round = maxit->round;
-
-            auto itr = _defi_market.begin();
-            while (itr != _defi_market.end() && itr->round < (max_round - 100))
-            {
-                _defi_market.erase(itr);
-                itr = _defi_market.begin();
-            }
-        }
 
         _defi_config.set(defi_config, _self);
     }
@@ -94,6 +201,7 @@ void onesgame::issue(name to, asset quantity, string memo)
 void onesgame::mineswap(name account, asset quantity)
 {
     require_auth(eosio::name(ONES_DEFI_ACCOUNT));
+    // eosio_assert(false, "onesgamemine contact is upgrading");
 
     eosio_assert(quantity.symbol == EOS_TOKEN_SYMBOL, "invalid symbol");
 
@@ -107,7 +215,8 @@ void onesgame::mineswap(name account, asset quantity)
 
     uint64_t cur_swap_quantity = defi_config.swap_quantity;
     uint64_t cur_time = now();
-    uint64_t increased_swap_quantity = (cur_time - defi_config.swap_time) * 0.02 * 10000;
+    uint64_t increased_swap_quantity =
+        (cur_time - defi_config.swap_time) * 0.02 * 10000;
 
     uint64_t total_swap_quantity = increased_swap_quantity + cur_swap_quantity;
 
@@ -120,10 +229,11 @@ void onesgame::mineswap(name account, asset quantity)
         t = total_swap_quantity - airdrop_swap_quantity;
     }
     defi_config.swap_counter += counter;
-    defi_config.swap_quantity = total_swap_quantity - airdrop_swap_quantity;
+    defi_config.swap_quantity =
+        total_swap_quantity - airdrop_swap_quantity;
     defi_config.swap_suply += airdrop_swap_quantity;
     uint64_t market_quantity = (airdrop_swap_quantity / 2);
-    defi_config.market_quantity += market_quantity;
+    defi_config.market_quantity[0] += market_quantity;
     defi_config.swap_time = cur_time;
 
     _defi_config.set(defi_config, _self);
@@ -140,50 +250,169 @@ void onesgame::mineswap(name account, asset quantity)
     {
         _defi_account.emplace(get_self(), [&](auto &t) {
             t.account = account;
-            t.market_quantity = asset(0, ONES_TOKEN_SYMBOL);
-            t.quantity = asset(0, ONES_TOKEN_SYMBOL);
+            init_amounts(t.market_quantity);
+            init_amounts(t.quantity);
             t.swap_quantity = asset(airdrop_swap_quantity, ONES_TOKEN_SYMBOL);
             t.mine_quantity = asset(quantity.amount, EOS_TOKEN_SYMBOL);
             t.timestamp = now();
         });
     }
 
-    this->_transfer_to(account, airdrop_swap_quantity, ONES_TOKEN_SYMBOL, "swap mine");
+    this->_transfer_to(account, airdrop_swap_quantity, ONES_TOKEN_SYMBOL,
+                       "swap mine");
 }
 
-void onesgame::minemarket(name account, uint64_t round, float_t factor)
+void onesgame::minemarkets()
 {
-    require_auth(get_self());
+    require_auth(name(ONES_PLAY_ACCOUNT));
+    st_defi_config defi_config = _defi_config.get();
+    eosio_assert(defi_config.market_quantity[0] > 10000, "market quantity is zero");
 
-    auto defi_market = _defi_market.find(round);
-    eosio_assert(defi_market != _defi_market.end(), "invalid round");
-    _defi_market.modify(defi_market, _self, [&](auto &t) {
-        t.executed += 1;
+    uint64_t round_id = 1;
+    auto it = _defi_market.rbegin();
+    if (it != _defi_market.rend())
+    {
+        round_id = it->round + 1;
+    }
+    auto itr = _defi_market.begin();
+    while (itr != _defi_market.end() && itr->round < (round_id - 100))
+    {
+        _defi_market.erase(itr);
+        itr = _defi_market.begin();
+    }
+    uint64_t total_amount = 0;
+    uint64_t total_user = 0;
+
+    _syncmarket(round_id, total_amount, total_user);
+
+    uint64_t cur_time = now();
+
+    _defi_market.emplace(get_self(), [&](auto &t) {
+        t.round = round_id;
+        t.amount = total_amount;
+        t.quantity = defi_config.market_quantity;
+        t.total = total_user;
+        t.executed = 0;
+        t.timestamp = cur_time;
     });
 
+    add_amounts(defi_config.market_suply, defi_config.market_quantity);
+    reset_amounts(defi_config.market_quantity);
+
+    _defi_config.set(defi_config, _self);
+
+    eosio::action(eosio::permission_level{get_self(), "active"_n},
+                  get_self(), "minemarket"_n,
+                  make_tuple(round_id))
+        .send();
+}
+
+void onesgame::_syncmarket(uint64_t round_id, uint64_t &total_amount, uint64_t &total_user)
+{
+    tb_defi_liquidity _defi_liquidity(name(ONES_DEFI_ACCOUNT),
+                                      name(ONES_DEFI_ACCOUNT).value);
+    auto lit = _defi_liquidity.begin();
+    map<uint64_t, float> liquidities;
+
+    while (lit != _defi_liquidity.end())
+    {
+        if (lit->liquidity_weight > 0)
+        {
+            liquidities[lit->liquidity_id] = lit->liquidity_weight;
+        }
+        lit++;
+    }
+
+    tb_defi_pool _defi_pool(name(ONES_DEFI_ACCOUNT),
+                            name(ONES_DEFI_ACCOUNT).value);
+    auto pit = _defi_pool.begin();
+
+    tb_defi_round _defi_round(get_self(), get_self().value);
+
+    std::map<uint64_t, float>::iterator mit;
+    uint64_t id = 1;
+    auto idx = _defi_round.rbegin();
+    if (idx != _defi_round.rend())
+    {
+        id = idx->id + 1;
+    }
+
+    while (pit != _defi_pool.end())
+    {
+        mit = liquidities.find(pit->liquidity_id);
+        if (mit != liquidities.end())
+        {
+            auto ait = _defi_round.find(pit->account.value);
+            uint64_t amount = pit->quantity1.amount * mit->second;
+            total_amount += amount;
+            _defi_round.emplace(get_self(), [&](auto &t) {
+                t.id = id++;
+                t.account = pit->account;
+                t.round_id = round_id;
+                t.amount = amount;
+            });
+            total_user++;
+        }
+        pit++;
+    }
+}
+void onesgame::minemarket(uint64_t round_id)
+{
+    require_auth(get_self());
+    // eosio_assert(false, "onesgamemine contact is upgrading");
+
+    auto market = _defi_market.find(round_id);
+    eosio_assert(market != _defi_market.end(), "invalid round");
+    eosio_assert(market->total > market->executed, "has been executed");
+
+    tb_defi_round _defi_round(get_self(), get_self().value);
+    auto round_index = _defi_round.get_index<"byroundkey"_n>();
+
+    auto it = round_index.find(round_id);
+    while (it != round_index.end())
+    {
+        float factor = (1.0 * it->amount / market->amount);
+        this->_minemarket(round_id, it->account, market->quantity, factor);
+        it = round_index.erase(it);
+    }
+
+    _defi_market.modify(market, _self, [&](auto &t) { t.executed = market->total; });
+}
+
+void onesgame::_minemarket(uint64_t round_id, name account, const vector<uint64_t> &quantity, float factor)
+{
     auto it = _defi_account.find(account.value);
 
-    uint64_t amount = defi_market->quantity.amount * factor;
     if (it != _defi_account.end())
     {
-        eosio_assert(round >= it->market_round, "invalid round");
+        eosio_assert(round_id >= it->market_round, "invalid round");
 
         _defi_account.modify(it, _self, [&](auto &t) {
-            t.quantity += asset(amount, ONES_TOKEN_SYMBOL);
-            t.market_round = round;
+            init_amounts(t.quantity);
+            for (uint64_t i = 0; i < quantity.size(); i++)
+            {
+                uint64_t amount = quantity[i] * factor;
+                t.quantity[i] += amount;
+            }
+            t.market_round = round_id;
         });
     }
     else
     {
-        eosio_assert(it->market_round != round, "market has been mined");
+        eosio_assert(it->market_round != round_id, "market has been mined");
 
         _defi_account.emplace(get_self(), [&](auto &t) {
             t.account = account;
-            t.quantity = asset(amount, ONES_TOKEN_SYMBOL);
-            t.market_quantity = asset(0, ONES_TOKEN_SYMBOL);
+            init_amounts(t.quantity);
+            init_amounts(t.market_quantity);
+            for (uint64_t i = 0; i < quantity.size(); i++)
+            {
+                uint64_t amount = quantity[i] * factor;
+                t.quantity[i] += amount;
+            }
             t.swap_quantity = asset(0, ONES_TOKEN_SYMBOL);
             t.mine_quantity = asset(0, EOS_TOKEN_SYMBOL);
-            t.market_round = round;
+            t.market_round = round_id;
             t.timestamp = now();
         });
     }
@@ -192,39 +421,21 @@ void onesgame::minemarket(name account, uint64_t round, float_t factor)
 void onesgame::claim(name account)
 {
     require_auth(account);
+    // eosio_assert(false, "onesgamemine contact is upgrading");
 
     auto it = _defi_account.find(account.value);
     eosio_assert((it != _defi_account.end()), "invalid account");
 
-    asset quantity = it->quantity;
+    vector<uint64_t> quantity = it->quantity;
     _defi_account.modify(it, _self, [&](auto &t) {
-        t.quantity -= quantity;
-        t.market_quantity += quantity;
+        reset_amounts(t.quantity);
+        add_amounts(t.market_quantity, quantity);
     });
 
-    this->_transfer_to(account, quantity.amount, ONES_TOKEN_SYMBOL, "market mine");
-}
-
-void onesgame::init()
-{
-    require_auth(get_self());
-    return;
-}
-
-void onesgame::_transfer_to(name to, uint64_t amount, symbol coin_code, string memo)
-{
-    if (amount == 0)
+    for (uint64_t i = 0; i < quantity.size(); i++)
     {
-        return;
-    }
-
-    if (coin_code == ONES_TOKEN_SYMBOL)
-    {
-        eosio::action(permission_level{get_self(), "active"_n},
-                      eosio::name(ONES_TOKEN_ACCOUNT),
-                      "transfer"_n,
-                      make_tuple(get_self(), to, asset(amount, ONES_TOKEN_SYMBOL), memo))
-            .send();
+        this->_transfer_to(account, quantity[i], token_symbols[i],
+                           "market mine");
     }
 }
 
@@ -234,24 +445,35 @@ extern "C"
     {
         if (action == eosio::name("onerror").value)
         {
-            eosio_assert(code == eosio::name("eosio").value, "onerror action’s are only valid from the eosio");
+            eosio_assert(code == eosio::name("eosio").value,
+                         "onerror action’s are only valid from the eosio");
         }
 
         if (code == receiver)
         {
             switch (action)
             {
-                EOSIO_DISPATCH_HELPER(onesgame, (mineswap)(minemarket)(claim)(init))
+                EOSIO_DISPATCH_HELPER(onesgame, (mineswap)(minemarket)(minemarkets)(
+                                                    claim)(init)(upgrade)(oauth))
             }
             return;
         }
 
-        if ((code == eosio::name(ONES_TOKEN_ACCOUNT).value) && action == eosio::name("issue").value)
+        if ((code == eosio::name(ONES_TOKEN_ACCOUNT).value) &&
+            action == eosio::name("issue").value)
         {
-            execute_action(eosio::name(receiver), eosio::name(receiver), &onesgame::issue);
+            execute_action(eosio::name(receiver), eosio::name(receiver),
+                           &onesgame::issue);
             return;
         }
 
+        if (code == eosio::name(BOX_TOKEN_ACCOUNT).value &&
+            action == eosio::name("transfer").value)
+        {
+            execute_action(eosio::name(receiver), eosio::name(receiver),
+                           &onesgame::transfer);
+            return;
+        }
         eosio_exit(0);
     }
 }
--- onesgamemine/onesgamemine.hpp
+++ onesgamemine/onesgamemine.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
-#include <eosiolib/asset.hpp>
 #include <eosiolib/crypto.h>
+
+#include <eosiolib/asset.hpp>
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/singleton.hpp>
 #include <eosiolib/time.hpp>
@@ -11,9 +12,7 @@
 using namespace eosio;
 using namespace std;
 
-// typedef std::vector<uint64_t> uint64Array;
-
-//contract
+// contract
 class [[eosio::contract("onesgamemine")]] onesgame : public contract
 {
 public:
@@ -34,13 +33,21 @@ public:
 
     [[eosio::action]] void mineswap(name account, asset quantity);
 
-    [[eosio::action]] void minemarket(name account, uint64_t round, float_t factor);
+    [[eosio::action]] void minemarket(uint64_t round_id);
+
+    [[eosio::action]] void minemarkets();
 
     [[eosio::action]] void claim(name account);
 
     [[eosio::action]] void init();
 
+    [[eosio::action]] void upgrade();
+
+    [[eosio::action]] void oauth(name account, uint8_t status);
+
     void issue(name to, asset quantity, string memo);
+
+    void transfer(name from, name to, asset quantity, string memo);
 
     struct [[eosio::table("config")]] st_defi_config
     {
@@ -49,10 +56,10 @@ public:
         uint64_t swap_suply;
         uint64_t swap_counter;
         uint64_t swap_issue;
-        uint64_t market_suply;
+        vector<uint64_t> market_suply;
         uint64_t market_time;
         uint64_t last_swap_suply;
-        uint64_t market_quantity;
+        vector<uint64_t> market_quantity;
         uint64_t market_issue;
     };
     typedef singleton<"config"_n, st_defi_config> tb_defi_config;
@@ -60,12 +67,14 @@ public:
     struct [[eosio::table]] st_defi_account
     {
         eosio::name account;
-        asset quantity;
-        asset market_quantity;
+        vector<uint64_t> quantity;
+        vector<uint64_t> market_quantity;
+
         asset swap_quantity;
         asset mine_quantity;
         uint64_t market_round;
         uint64_t timestamp;
+        uint8_t oauth;
 
         uint64_t primary_key() const { return account.value; }
     };
@@ -75,7 +84,8 @@ public:
     struct [[eosio::table]] st_defi_market
     {
         uint64_t round;
-        asset quantity;
+        uint64_t amount;
+        vector<uint64_t> quantity;
         uint64_t total;
         uint64_t executed;
         uint64_t timestamp;
@@ -85,12 +95,91 @@ public:
 
     typedef multi_index<"market"_n, st_defi_market> tb_defi_market;
 
+    struct [[eosio::table]] st_defi_round
+    {
+        uint64_t id;
+        eosio::name account;
+        uint64_t round_id;
+
+        uint64_t amount;
+
+        uint64_t primary_key() const { return id; }
+        // uint64_t account_key() const { return account.value; }
+        uint64_t round_key() const { return round_id; }
+    };
+
+    typedef multi_index<
+        "round"_n, st_defi_round,
+        // indexed_by<"bykey"_n, const_mem_fun<st_defi_round, uint64_t,
+        //  &st_defi_round::round_key>>,
+        indexed_by<"byroundkey"_n, const_mem_fun<st_defi_round, uint64_t,
+                                                 &st_defi_round::round_key>>>
+        tb_defi_round;
+
+    struct token_t
+    {
+        name address;
+        symbol symbol;
+        friend bool operator==(const token_t &a, const token_t &b)
+        {
+            return a.address == b.address && a.symbol == b.symbol;
+        }
+        EOSLIB_SERIALIZE(token_t, (address)(symbol))
+    };
+
+    struct st_defi_pool
+    {
+        uint64_t pool_id;
+        eosio::name account;
+        uint64_t liquidity_id;
+        uint64_t liquidity_token;
+        eosio::asset quantity1;
+        eosio::asset quantity2;
+        uint64_t timestamp;
+
+        uint64_t primary_key() const { return pool_id; }
+        uint64_t account_key() const { return account.value; }
+    };
+
+    typedef multi_index<
+        "defipool"_n, st_defi_pool,
+        indexed_by<"byaccountkey"_n, const_mem_fun<st_defi_pool, uint64_t,
+                                                   &st_defi_pool::account_key>>>
+        tb_defi_pool;
+
+    struct st_defi_liquidity
+    {
+        uint64_t liquidity_id;
+        token_t token1;
+        token_t token2;
+
+        eosio::asset quantity1;
+        eosio::asset quantity2;
+        uint64_t liquidity_token;
+        float_t price1;
+        float_t price2;
+        uint64_t cumulative1;
+        uint64_t cumulative2;
+        float_t swap_weight;
+        float_t liquidity_weight;
+        uint64_t timestamp;
+
+        uint64_t primary_key() const { return liquidity_id; }
+    };
+
+    typedef multi_index<"liquidity"_n, st_defi_liquidity> tb_defi_liquidity;
+
 private:
     void _transfer_to(name to, uint64_t amount, symbol coin_code, string memo);
+
+    void _syncmarket(uint64_t round_id, uint64_t & total_amount, uint64_t & total_user);
+
+    void _minemarket(uint64_t round_id, name account, const vector<uint64_t> &quantity, float factor);
 
 private:
     tb_defi_account _defi_account;
 
     tb_defi_config _defi_config;
+
     tb_defi_market _defi_market;
 };
